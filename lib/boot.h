2835bd8b264d52dc922cc303c0c5e5ddb6aedb99
Amlal <amlal@nekernel.org>
2025-08-16 19:35:12 +0200

    fix: lib/boot.h: use portable types for gcc/clang.
    Signed-off-by: Amlal <amlal@nekernel.org>

--- lib/boot.h
+++ lib/boot.h
@@ -31,8 +31,8 @@ typedef unsigned char          uint8_t;
 typedef __INTPTR_TYPE__ intptr_t;
 
 typedef __INT64_TYPE__ int64_t;
-typedef int            int32_t;
-typedef short          int16_t;
+typedef __INT32_TYPE__ int32_t;
+typedef __INT16_TYPE__ int16_t;
 typedef char           int8_t;
 
 typedef void*       voidptr_t;
