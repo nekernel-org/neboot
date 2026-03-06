e9eb93ced36628f2c111b96c4916bb649636e71c
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-06 05:54:15 +0100

    [CHORE] Source code level refactors and improvements.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- src/neboot-bootnet.c
+++ src/neboot-bootnet.c
@@ -1,4 +1,4 @@
-// Copyright 2025, Amlal El Mahrouss (amlal@nekernel.org)
+// Copyright 2025-2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Distributed under the Apache Software License, Version 2.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.apache.org/licenses/LICENSE-2.0)
 // Official repository: https://github.com/ne-foss-org/neboot
